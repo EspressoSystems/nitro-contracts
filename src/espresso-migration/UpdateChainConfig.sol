59d425da45de45cbc3205d098dbdc92a86bbbae0
Zach Showalter <zacshowa@gmail.com>
2024-08-05 19:34:03 -0400

    Fixes to UpdateChainConfig contract.

--- src/espresso-migration/UpdateChainConfig.sol
+++ src/espresso-migration/UpdateChainConfig.sol
@@ -1,13 +1,14 @@
+pragma solidity ^0.8.9;
 // Example action contract to upgrade chain config to turn on espresso mode
 
 import "../precompiles/ArbOwner.sol";
 
 contract SetEspressoChainConfig {
-    function perform(string serialiazedEspressoConfig) external {
+    function perform(string calldata serialiazedEspressoConfig) public {
         //The ArbOwner precomiple always lives at this addr.
-        ArbOwner arbOwner = ArbOwner(0x0000000000000000000000000000000000000070)
+        ArbOwner arbOwner = ArbOwner(0x0000000000000000000000000000000000000070);
         //This call must come from an account designated as an Owner by the ArbOwner contract.
         //In practice the Owner should be the UpgradeExecutor via the execute method. 
-        arbOwner.setChainConfig(serialiazedEspressoConfig)
+        arbOwner.setChainConfig(serialiazedEspressoConfig);
     }
 }
