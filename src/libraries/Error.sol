7cc493051189ddbc17ca36beb2e4b6f4a8cf0c5b
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2024-11-20 11:35:52 -0500

    merge

--- src/libraries/Error.sol
+++ src/libraries/Error.sol
@@ -202,3 +202,6 @@ error BadMaxTimeVariation();
 
 /// @dev Thrown when Blobstream verification fails for a Celestia Data Root
 error InvalidCelestiaBatch();
+
+/// @dev Thrown when the TEE Attestation quote is invalid
+error InvalidTEEAttestationQuote();
