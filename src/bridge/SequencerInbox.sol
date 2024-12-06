1a5727da5fe15e3b8b4635c1ea93a43db0793e3a
Sneh Koul <snehkoul1999@gmail.com>
2024-12-06 16:13:28 -0500

    remove unwanted changes

--- src/bridge/SequencerInbox.sol
+++ src/bridge/SequencerInbox.sol
@@ -47,7 +47,7 @@ import {IGasRefunder} from "../libraries/IGasRefunder.sol";
 import {GasRefundEnabled} from "../libraries/GasRefundEnabled.sol";
 import "../libraries/ArbitrumChecker.sol";
 import {IERC20Bridge} from "./IERC20Bridge.sol";
-import {EspressoTEEVerifier} from "../bridge/EspressoTEEVerifier.sol";
+import {IEspressoTEEVerifier} from "../bridge/IEspressoTEEVerifier.sol";
 
 /**
  * @title  Accepts batches from the sequencer and adds them to the rollup inbox.
@@ -132,7 +132,7 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
     // True if the chain this SequencerInbox is deployed on uses custom fee token
     bool public immutable isUsingFeeToken;
 
-    EspressoTEEVerifier public espressoTEEVerifier;
+    IEspressoTEEVerifier public espressoTEEVerifier;
 
     constructor(uint256 _maxDataSize, IReader4844 reader4844_, bool _isUsingFeeToken) {
         maxDataSize = _maxDataSize;
@@ -185,7 +185,10 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
         Deprecated because we created another `initialize` function that accepts the `EspressoTEEVerifier` contract
         address as a parameter which is used by the `SequencerInbox` contract to verify the TEE attestation quote.
      */
-    function initialize(IBridge bridge_, ISequencerInbox.MaxTimeVariation calldata maxTimeVariation_) external onlyDelegated {
+    function initialize(
+        IBridge bridge_,
+        ISequencerInbox.MaxTimeVariation calldata maxTimeVariation_
+    ) external onlyDelegated {
         revert Deprecated();
     }
 
@@ -213,7 +216,7 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
         rollup = bridge_.rollup();
 
         _setMaxTimeVariation(maxTimeVariation_);
-        espressoTEEVerifier = EspressoTEEVerifier(_espressoTEEVerifier);
+        espressoTEEVerifier = IEspressoTEEVerifier(_espressoTEEVerifier);
     }
 
     /// @notice Allows the rollup owner to sync the rollup address
@@ -856,7 +859,7 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
     }
 
     function setEspressoTEEVerifier(address _espressoTEEVerifier) external onlyRollupOwner {
-        espressoTEEVerifier = EspressoTEEVerifier(_espressoTEEVerifier);
+        espressoTEEVerifier = IEspressoTEEVerifier(_espressoTEEVerifier);
         emit OwnerFunctionCalled(6);
     }
 
