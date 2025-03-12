f3fae4fd27621592c08c477dad790b4f86c1a6c1
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2025-03-12 18:04:57 -0400

    Fix Mock Contract (#51)
    * Fix Mock Contract
    
    * fix comment

--- src/mocks/EspressoTEEVerifier.sol
+++ src/mocks/EspressoTEEVerifier.sol
@@ -24,11 +24,11 @@ contract EspressoTEEVerifierMock {
         bytes calldata data,
         TeeType teeType
     ) external {
-        // data length should be 32 bytes
-        require(data.length == 32, "Invalid data length");
-        // Convert the data to bytes32 and pass it to the verify function
-        address signerFromReport = address(uint160(uint256(keccak256(data[:20]))));
-        registeredSigner[signerFromReport] = true;
+        // data length should be 20 bytes
+        require(data.length == 20, "Invalid data length");
+
+        address signer = address(uint160(bytes20(data[:20])));
+        registeredSigner[signer] = true;
     }
 
     function registeredSigners(address signer, TeeType teeType) external view returns (bool) {
