1b0497333f3eeb335ba18ed64040ed9f62933367
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2025-03-12 17:29:33 -0400

    Support multiple MR_ENCLAVE and MR_SIGNER (#50)
    * Support multiple MR_ENCLAVE and MR_SIGNER
    
    * fix interface
    
    * fix interface
    
    * fix emit

--- src/bridge/EspressoTEEVerifier.sol
+++ src/bridge/EspressoTEEVerifier.sol
@@ -25,20 +25,18 @@ import {IEspressoTEEVerifier} from "./IEspressoTEEVerifier.sol";
  *         from automata to verify the quote. Along with some additional verification logic.
  */
 contract EspressoTEEVerifier is IEspressoTEEVerifier, Ownable2Step {
-    event MREnclaveSet(bytes32 indexed mrEnclave);
-    event MRSignerSet(bytes32 indexed mrSigner);
-
     using BytesUtils for bytes;
 
     // V3QuoteVerififer contract from automata to verify the quote
     V3QuoteVerifier public quoteVerifier;
-    bytes32 public mrEnclave;
-    bytes32 public mrSigner;
+
+    mapping(bytes32 => bool) public mrEnclaves;
+    mapping(bytes32 => bool) public mrSigners;
 
     constructor(bytes32 _mrEnclave, bytes32 _mrSigner, address _quoteVerifier) {
         quoteVerifier = V3QuoteVerifier(_quoteVerifier);
-        mrEnclave = _mrEnclave;
-        mrSigner = _mrSigner;
+        mrEnclaves[_mrEnclave] = true;
+        mrSigners[_mrSigner] = true;
     }
 
     /*
@@ -70,8 +68,7 @@ contract EspressoTEEVerifier is IEspressoTEEVerifier, Ownable2Step {
             revert FailedToParseEnclaveReport();
         }
 
-        // Check that mrEnclave and mrSigner match
-        if (localReport.mrEnclave != mrEnclave || localReport.mrSigner != mrSigner) {
+        if (!mrEnclaves[localReport.mrEnclave] || !mrSigners[localReport.mrSigner]) {
             revert InvalidMREnclaveOrSigner();
         }
 
@@ -129,16 +126,24 @@ contract EspressoTEEVerifier is IEspressoTEEVerifier, Ownable2Step {
     /*
      * @dev Set the mrEnclave of the contract
      */
-    function setMrEnclave(bytes32 _mrEnclave) external onlyOwner {
-        emit MREnclaveSet(_mrEnclave);
-        mrEnclave = _mrEnclave;
+    function setMrEnclave(bytes32 _mrEnclave, bool _isValid) external onlyOwner {
+        if (_isValid) {
+            mrEnclaves[_mrEnclave] = true;
+        } else {
+            delete mrEnclaves[_mrEnclave];
+        }
+        emit MREnclaveSet(_mrEnclave, _isValid);
     }
 
     /*
      * @dev Set the mrSigner of the contract
      */
-    function setMrSigner(bytes32 _mrSigner) external onlyOwner {
-        emit MRSignerSet(_mrSigner);
-        mrSigner = _mrSigner;
+    function setMrSigner(bytes32 _mrSigner, bool _isValid) external onlyOwner {
+        if (_isValid) {
+            mrSigners[_mrSigner] = true;
+        } else {
+            delete mrSigners[_mrSigner];
+        }
+        emit MRSignerSet(_mrSigner, _isValid);
     }
 }
