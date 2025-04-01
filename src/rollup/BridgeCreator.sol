5fcdbc4d8f15473828a5804634e2b16d58ddca59
Sneh Koul <snehkoul1999@gmail.com>
2025-04-02 02:25:50 +0530

    remove unwanted deprecated function

--- src/rollup/BridgeCreator.sol
+++ src/rollup/BridgeCreator.sol
@@ -123,22 +123,6 @@ contract BridgeCreator is Ownable {
     return frame;
   }
 
-  /*
-   * Deprecated because we added a new method to create bridges
-   * which requires the address of the `EspressoTEEVerifier` contract
-   * to be passed in. `EspressoTEEVerifier` is required by the sequencer
-   * inbox contract to verify the quote from the TEE to check if the batch has
-   * been posted by a batch poster running in the TEE.
-   */
-  function createBridge(
-    address adminProxy,
-    address rollup,
-    address nativeToken,
-    ISequencerInbox.MaxTimeVariation calldata maxTimeVariation
-  ) external returns (BridgeContracts memory) {
-    revert Deprecated();
-  }
-
   function createBridge(
     address adminProxy,
     address rollup,
