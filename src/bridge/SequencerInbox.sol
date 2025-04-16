93da05cfb52f319ea084b565312143c25446fe49
Sneh Koul <snehkoul1999@gmail.com>
2025-04-17 01:19:55 +0530

    Support blobs in v3.0.0

--- src/bridge/SequencerInbox.sol
+++ src/bridge/SequencerInbox.sol
@@ -423,10 +423,39 @@ contract SequencerInbox is
     uint256 prevMessageCount,
     uint256 newMessageCount
   ) external refundsGas(gasRefunder, reader4844) {
+    revert Deprecated();
+  }
+
+  /// @inheritdoc ISequencerInbox
+  function addSequencerL2BatchFromBlobs(
+    uint256 sequenceNumber,
+    uint256 afterDelayedMessagesRead,
+    IGasRefunder gasRefunder,
+    uint256 prevMessageCount,
+    uint256 newMessageCount,
+    bytes memory quote
+  ) external refundsGas(gasRefunder, reader4844) {
     if (!isBatchPoster[msg.sender]) revert NotBatchPoster();
     if (isDelayProofRequired(afterDelayedMessagesRead))
       revert DelayProofRequired();
 
+    bytes32[] memory dataHashes = reader4844.getDataHashes();
+    if (dataHashes.length == 0) revert MissingDataHashes();
+    // take keccak2256 hash of all the function arguments and encode packed blob hashes
+    // except the quote
+    bytes32 reportDataHash = keccak256(
+      abi.encode(
+        sequenceNumber,
+        afterDelayedMessagesRead,
+        address(gasRefunder),
+        prevMessageCount,
+        newMessageCount,
+        abi.encodePacked(dataHashes)
+      )
+    );
+    // verify the quote for the batch poster running in the TEE
+    espressoTEEVerifier.verify(quote, reportDataHash);
+    emit TEEAttestationQuoteVerified(sequenceNumber);
     addSequencerL2BatchFromBlobsImpl(
       sequenceNumber,
       afterDelayedMessagesRead,
