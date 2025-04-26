7cdde3176a5858f22dfb96de4021954e8954c3c6
Sneh Koul <snehkoul1999@gmail.com>
2025-04-26 22:36:52 +0530

    lint

--- src/mocks/EspressoTEEVerifierBlobsMock.sol
+++ src/mocks/EspressoTEEVerifierBlobsMock.sol
@@ -53,7 +53,7 @@ contract EspressoTEEVerifierBlobsMock is IEspressoTEEVerifier {
       revert FailedToParseEnclaveReport();
     }
 
-    //  Verify that the reportDataHash if the hash signed by the TEE
+    // Verify that the reportDataHash if the hash signed by the TEE
     // We do not check the signature because `quoteVerifier.verifyQuote` already does that
     if (reportDataHash != bytes32(localReport.reportData.substring(0, 32))) {
       revert InvalidReportDataHash();
