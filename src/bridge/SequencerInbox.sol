d13efe22ce11ab4dbae25a6cc0cb4a358f031213
Luke Iannucci <luke.iannucci@outlook.com>
2025-09-05 16:58:55 -0400

    changes

--- src/bridge/SequencerInbox.sol
+++ src/bridge/SequencerInbox.sol
@@ -65,7 +65,7 @@ import '../libraries/ArbitrumChecker.sol';
 import { IERC20Bridge } from './IERC20Bridge.sol';
 import './DelayBuffer.sol';
 import {IEspressoTEEVerifier} from "espresso-tee-contracts/interface/IEspressoTEEVerifier.sol";
-
+import {KeyManager} from "timeboost-contracts/KeyManager.sol";
 /**
  * @title  Accepts batches from the sequencer and adds them to the rollup inbox.
  * @notice Contains the inbox accumulator which is the ordering of all data and transactions to be processed by the rollup.
