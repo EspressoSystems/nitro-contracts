3cccba38bb823670594cf171c2e9b528a95ae2e6
Jeremy <yiliang.he@qq.com>
2026-02-05 11:29:45 +0800

    Address comments
    (cherry picked from commit 35bb6fd0255101ec0beb3c2eb388851eaef75ed4)

--- test/foundry/EspressoTEEVerifierMock.t.sol
+++ test/foundry/EspressoTEEVerifierMock.t.sol
@@ -1,30 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-enum ServiceType {
-    BatchPoster,
-    CaffNode
-}
+import "../../src/bridge/EspressoTEE.sol";
 
-contract EspressoTEEVerifierMock {
-    enum TeeType {
-        SGX,
-        NITRO
-    }
-
+contract EspressoTEEVerifierMock is IEspressoTEEVerifier {
     /**
-     * @notice Verify signature from a registered signer
+     * @notice Verify signature from a registered signer. This mock always returns true.
      * @param signature The signature of the user data
      * @param userDataHash The hash of the user data
      * @param teeType The type of TEE
+     * @param service The type of service
      */
     function verify(
         bytes memory signature,
         bytes32 userDataHash,
         TeeType teeType,
         ServiceType service
-    ) external view returns (bool) {
+    ) external view override returns (bool) {
+        // Unused parameters to avoid compiler warnings.
+        (signature, userDataHash, teeType, service);
         return true;
     }
-
 }
