e5c355faa23bc76400c1aef0abf4e446b1a913ad
Sneh Koul <snehkoul1999@gmail.com>
2026-06-01 17:42:47 -0400

    fix mock

--- test/foundry/EspressoTEEVerifierMock.t.sol
+++ test/foundry/EspressoTEEVerifierMock.t.sol
@@ -29,7 +29,7 @@ contract EspressoTEEVerifierMock is IEspressoTEEVerifier {
     }
 
     function isSignerValid(address, TeeType) external view returns (bool) {
-        return false;
+        return true;
     }
 
     function setEspressoNitroTEEVerifier(IEspressoNitroTEEVerifier) external {}
